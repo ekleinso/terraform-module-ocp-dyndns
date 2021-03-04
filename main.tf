fc256e93a1f8dd1d4d3dd5afd9fa16e5ab8b03a0
Eric Kleinsorgen <emk@us.ibm.com>
2021-03-04 02:10:22 -0500

    fixed arec

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "dns_a_record_set" "cluster" {
   for_each = var.hostnames_ip_addresses
 
   zone = "${var.dns_domain}."
-  name = format("%s.%s", element(split(each.key, "."), 0), var.cluster_name)
+  name = format("%s.%s", element(split(".", each.key), 0), var.cluster_name)
   addresses = [
     each.value
   ]
